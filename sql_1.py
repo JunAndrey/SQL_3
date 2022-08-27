91ebd84006a241a11b04ebcd5e3ba317a10acdd8
JunAndrey <106904007+JunAndrey@users.noreply.github.com>
2022-08-27 22:06:15 +0300

    Update sql_1.py

--- sql_1.py
+++ sql_1.py
@@ -1,39 +1,32 @@
 import psycopg2
 from psycopg2 import OperationalError
 
+import config
+from config import username_3, password_3, danabase_3
 
 
-def create_connection(db_name, db_user, db_password):
+
+def create_connection():
     try:
         conn = psycopg2.connect(
-            database=db_name,
-            user=db_user,
-            password=db_password,
-        )
+            database=config.danabase_3,
+            user=config.username_3,
+            password=config.password_3)
+        cur = conn.cursor()
         print("Connection to PostgreSQL DB successful")
+        return cur, conn
+
     except OperationalError as err:
         print(f"The error '{err}' occurred")
-    return conn
-
-
-conn = create_connection("clients_db", "postgres", "...")
 
 
 
+def create_db(cur, conn, query):
 
-with conn.cursor() as cur:
-    cur.execute("""
-                DROP TABLE phones;
-                DROP TABLE clients;
-                """)
-
-
-def create_db(conn, query):
-    cur = conn.cursor()
     try:
         cur.execute(query)
         conn.commit()
-        print("Query executed successfully")
+        print("Table created")
     except OperationalError as err:
         print(f"The error '{err}' occurred")
 
@@ -47,7 +40,7 @@ create_clients_table = """
     )
     """
 
-create_db(conn, create_clients_table)
+
 create_phones_table = """
     CREATE TABLE IF NOT EXISTS phones(
     phone_id INTEGER PRIMARY KEY,
@@ -56,82 +49,86 @@ create_phones_table = """
     )
     """
 
-create_db(conn, create_phones_table)
 
-def add_client(conn, in_clients):
+def add_client(cur, conn):
     for client in in_clients:
-        query = '''
-                    INSERT INTO clients (client_id, name, surname, email)
-                    VALUES (%s, '%s', '%s', '%s');
-                    ''' % (client[0], client[1], client[2], client[3])
-        create_db(conn, query)
+        try:
+            insert_table_clients = '''
+                INSERT INTO clients (client_id, name, surname, email)
+                VALUES (%s, '%s', '%s', '%s');
+                ''' % (client[0], client[1], client[2], client[3])
+            create_db(cur, conn, insert_table_clients)
+            conn.commit()
+            print("Table Clients inserted")
+        except OperationalError as err:
+            print(f"The error '{err}' occurred")
 
 in_clients = [
     (1,'Ivan', 'Ivanov', 'IvIvanov@mail.org.ru'),
     (2, 'Oleg', 'Petrov', 'Oleg1petrov@mail.ru'),
-    (3, 'Alex', 'Markov', 'AlexMarkov@gmail.com')
+    (3, 'Alex', 'Markov', 'AlexMarkov@gmail.com'),
  ]
-add_client(conn, in_clients)
 
-def add_phones(conn, in_phones):
+
+def add_phones(cur, conn):
     for phone in in_phones:
-        query = '''
-                    INSERT INTO phones (phone_id, client_id, phone_number)
-                    VALUES (%s, '%s', '%s');
-                    ''' % (phone[0], phone[1], phone[2])
-        create_db(conn, query)
-
+        try:
+            insert_table_phones = '''
+                INSERT INTO phones (phone_id, client_id, phone_number)
+                VALUES (%s, '%s', '%s');
+                ''' % (phone[0], phone[1], phone[2])
+            create_db(cur, conn, insert_table_phones)
+            conn.commit()
+            print("Table Phones inserted")
+        except OperationalError as err:
+            print(f"The error '{err}' occurred")
 in_phones = [
     (1, 1, '+76051223322'),
     (2, 1, '+78225122333'),
     (3, 2, '+79213338899'),
     (4, 3, '+79214446677'),
  ]
-add_phones(conn, in_phones)
 
-def change_client(conn, ch_client):
+def change_client(cur,conn, ch_client):
     ch_client = ch_client[0]
-    query = '''
+    in_change_client = '''
             UPDATE clients 
             SET %s = '%s'
             WHERE client_id = %s
             ''' % (ch_client[1], ch_client[2], ch_client[0])
-    create_db(conn, query)
+    cur.execute(in_change_client)
+    conn.commit()
 
 ch_client = [
     (3, 'name', 'Nero'),
 ]
-change_client(conn, ch_client)
 
-def del_phone(conn, client_id):
+
+def del_phone(cur, conn, client_id):
     query = '''
             DELETE FROM phones
             WHERE client_id = %s
             ''' %(client_id)
-    create_db(conn, query)
+    create_db(cur, conn, query)
 
-del_phone(conn, 3)
 
-def del_client(conn, client_id):
-    cur = conn.cursor()
-    cur.execute('''
-        
+def del_client(cur, conn, client_id):
+    query_1 = '''
         DELETE FROM phones
-        WHERE client_id = %s
-        ''' % (client_id))
-    conn.commit()
-    query = '''
-        DELETE FROM clients
-        WHERE client_id = %s
+        WHERE client_id = %s;
         ''' % (client_id)
-    create_db(conn, query)
+    create_db(cur, conn, query_1)
+    conn.commit()
+    query_2 = '''
+        DELETE FROM clients
+        WHERE client_id = %s;
+        ''' % (client_id)
+    create_db(cur, conn, query_2)
     conn.commit()
 
-del_client(conn, 1)
 
-def searh_client(conn, cur, sear_clie):
+def searh_client(cur, sear_clie):
     sear_clie = sear_clie[0]
-    cur = conn.cursor()
 
     try:
         cur.execute('''
@@ -148,6 +145,19 @@ def searh_client(conn, cur, sear_clie):
 sear_clie = [
      ('surname', 'Petrov'),
  ]
-searh_client(conn, cur, sear_clie)
+
+
+if __name__ == '__main__':
+    cur = create_connection()[0]
+    conn = create_connection()[1]
+    create_db(conn.cursor(), conn, create_clients_table)
+    create_db(conn.cursor(), conn, create_phones_table)
+    add_client(conn.cursor(), conn)
+    add_phones(conn.cursor(), conn)
+    change_client(conn.cursor(), conn, ch_client)
+    del_phone(conn.cursor(), conn, 3)
+    searh_client(conn.cursor(), sear_clie)
+
+
 
 conn.close()
